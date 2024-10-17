5a9d0e06d744939adcf0c33ba9e498ee80bb7342
jzhao602 <jzhao602@gatech.edu>
2024-10-17 17:31:18 -0400

    updated file for git publish

--- jingzhe_personal_web/vue.config.js
+++ jingzhe_personal_web/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/JingzheZhao.github.io/'  // Change this to your GitHub repo name
+    : '/',
   transpileDependencies: true
 })
