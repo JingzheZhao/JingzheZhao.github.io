d38a9c212c6e73e785fe6cae95cae04fe7f4a6aa
jzhao602 <jzhao602@gatech.edu>
2025-09-14 23:01:49 -0400

    add .gitignore to ignore node_modules and build files

--- Jingzhe_Personal_Web/src/components/Banner.js
+++ Jingzhe_Personal_Web/src/components/Banner.js
@@ -1,7 +1,30 @@
 import React from 'react';
+// images
+import Image from '../assets/avatar.svg';
+// icons
+import {FaGithub, FaLinkedinIn} from 'react-icons/fa';
+// type animation
+import {TypeAnimation} from 'react-type-animation';
+// motion
+import {motion} from 'framer-motion';
+// variants
+import {fadeIn} from '../variants';
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className='section' id='home'>
+      <div className='container mx-auto'>
+        <div>
+          {/* text */}
+          <div>text</div>
+          {/* image */}
+          <div>
+            <img src={Image} alt=''/>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
